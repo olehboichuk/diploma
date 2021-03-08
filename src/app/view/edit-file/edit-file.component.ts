cd330b63fe1f99de3dd2007138b1b1ae668ef846
Oleg <olegboychuk099@gmail.com>
2021-03-08 20:34:05 +0200

    add invite link

--- src/app/view/edit-file/edit-file.component.ts
+++ src/app/view/edit-file/edit-file.component.ts
@@ -67,4 +67,19 @@ export class EditFileComponent implements OnInit, OnDestroy {
   goToChat(): void {
     this.fileSocketService.getFile(this.fileId);
   }
+
+  copyInputMessage(val: string): void {
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+
 }
